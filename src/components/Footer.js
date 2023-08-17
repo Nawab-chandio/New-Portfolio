6bd386bf983a229a96382f61777136b8421e10bf
Nawab Ali <nawab.ali@growtharbor.com>
2023-08-17 11:55:56 +0500

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import Link from "@mui/material/Link";
+import { Box, Grid, Typography } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 function Footer() {
   return (
-    <div></div>
-  )
+    <Grid
+      sx={{
+        backgroundColor: "#2c405e",
+        p: 4,
+      }}
+      component="footer"
+      width={"100%"}
+    >
+      <Grid container>
+        <Grid
+          item
+          xs={4}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "5px",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "white",
+              fontFamily: "lexend",
+            }}
+            variant= "h5" 
+          >
+            Social Media Contacts
+          </Typography>
+          <Link href="#" style={{ color: "white" }}>
+            <FacebookIcon />
+          </Link>
+          <Link href="#" style={{ color: "white" }}>
+            <InstagramIcon />
+          </Link>
+          <Link href="#" style={{ color: "white" }}>
+            <LinkedInIcon />
+          </Link>
+          <Link href="#" style={{ color: "white" }}>
+            <TwitterIcon />
+          </Link>
+        </Grid>
+        <Grid item xs={8}>
+          <Typography variant="body2" color="white" align="end">
+            {"Copyright © "}
+            <Link color="inherit" href="#">
+              Your Website
+            </Link>{" "}
+            {new Date().toLocaleString("en-US", {
+              year: "numeric",
+              month: "long",
+            })}
+            {"."}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -3,97 +3,103 @@ import React from "react";
 import Nawab from "./../pics/Nawab.jpeg";
 import Projects from "./Projects";
 import Skills from "./Skills";
+import Footer from "./Footer";
 
 function MainBody() {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={4}>
-        <Card
-          sx={{
-            display: "inline-block",
-            boxShadow: "rgba(0,0,0,0.2)",
-            borderRadius: 2,
-            margin: 1.5,
-            backgroundColor: "#2c405e",
-            width: "100%",
-            height: 350,
-            transition: "box-shadow 0.3s",
-            "&:hover": {
-              boxShadow: "0px 4px 10px rgba(0,0,0,0.7)",
-            },
-          }}
-        >
-          <Typography
+    <Grid container >
+      <Grid item container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Card
             sx={{
-              color: "white",
-              fontFamily: "lexend",
-              padding: "6px",
-              fontSize: {
-                xs: "17.5px", // Font size for screens below 600px width
-                sm: "20px", // Font size for screens 600px width and above
+              display: "inline-block",
+              boxShadow: "rgba(0,0,0,0.2)",
+              borderRadius: 2,
+              margin: 1.5,
+              backgroundColor: "#2c405e",
+              width: "100%",
+              height: 350,
+              transition: "box-shadow 0.3s",
+              "&:hover": {
+                boxShadow: "0px 4px 10px rgba(0,0,0,0.7)",
               },
             }}
           >
-            {"I'm a front-end developer at "}
-            <span style={{ color: "brown", fontStyle: "italic" }}>
-              Growth arbor Pvt
-            </span>
-            {
-              ", contributing to an exciting project. As part of the development team, I play a key role in creating user-friendly websites and applications using technologies such as HTML, CSS, JavaScript, React JS, and MUI (Material-UI). I am passionate about continuously improving my skills and keeping up-to-date with the latest web technologies by reading web tech articles on various online platforms. Through my dedication and expertise, I aim to contribute to the project's success and create impactful solutions for our users."
-            }
-          </Typography>
-        </Card>
-      </Grid>
+            <Typography
+              sx={{
+                color: "white",
+                fontFamily: "lexend",
+                padding: "6px",
+                fontSize: {
+                  xs: "17.5px", // Font size for screens below 600px width
+                  sm: "20px", // Font size for screens 600px width and above
+                },
+              }}
+            >
+              {"I'm a front-end developer at "}
+              <span style={{ color: "brown", fontStyle: "italic" }}>
+                Growth arbor Pvt
+              </span>
+              {
+                ", contributing to an exciting project. As part of the development team, I play a key role in creating user-friendly websites and applications using technologies such as HTML, CSS, JavaScript, React JS, and MUI (Material-UI). I am passionate about continuously improving my skills and keeping up-to-date with the latest web technologies by reading web tech articles on various online platforms. Through my dedication and expertise, I aim to contribute to the project's success and create impactful solutions for our users."
+              }
+            </Typography>
+          </Card>
+        </Grid>
 
-      <Grid item xs={12} sm={5} pl={{ xs: "0", sm: "50px" }}>
-        <Card
-          sx={{
-            display: "inline-block",
-            boxShadow: "rgba(0,0,0,0.2)",
-            borderRadius: 2,
-            margin: 1.5,
-            backgroundColor: "#2c405e",
-            width: "100%",
-            height: 350,
-            transition: "box-shadow 0.3s",
-            "&:hover": {
-              boxShadow: "0px 4px 10px rgba(0,0,0,0.7)",
-              cursor: "pointer",
-            },
-          }}
-        >
-          <Typography>
-            <Skills />
-          </Typography>
-        </Card>
-      </Grid>
-      <Grid item xs={12} sm={3} sx={{ display: "flex", alignItems: "center" }}>
-        <Card
-          sx={{
-            display: "inline-block",
-            boxShadow: "rgba(0,0,0,0.2)",
-            borderRadius: 10,
-            margin: 1.5,
-            backgroundColor: "#2c405e",
-            width: "100%",
-            height: 350,
-            transition: "box-shadow 0.3s",
-            "&:hover": {
-              boxShadow: "0px 4px 10px rgba(0,0,0,0.7)",
-              cursor: "pointer",
-            },
-          }}
-        >
-          <img
-            src={Nawab}
-            alt="Picture"
-            style={{ width: "100%", height: "100%", objectFit: "cover" }}
-          />
-        </Card>
+        <Grid item xs={12} sm={5} pl={{ xs: "0", sm: "50px" }}>
+          <Card
+            sx={{
+              display: "inline-block",
+              boxShadow: "rgba(0,0,0,0.2)",
+              borderRadius: 2,
+              margin: 1.5,
+              backgroundColor: "#2c405e",
+              width: "100%",
+              height: 350,
+              transition: "box-shadow 0.3s",
+              "&:hover": {
+                boxShadow: "0px 4px 10px rgba(0,0,0,0.7)",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <Typography>
+              <Skills />
+            </Typography>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={3} sx={{ display: "flex", alignItems: "center" }}>
+          <Card
+            sx={{
+              display: "inline-block",
+              boxShadow: "rgba(0,0,0,0.2)",
+              borderRadius: 10,
+              margin: 1.5,
+              backgroundColor: "#2c405e",
+              width: "100%",
+              height: 350,
+              transition: "box-shadow 0.3s",
+              "&:hover": {
+                boxShadow: "0px 4px 10px rgba(0,0,0,0.7)",
+                cursor: "pointer",
+              },
+            }}
+          >
+            <img
+              src={Nawab}
+              alt="Picture"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+          </Card>
+        </Grid>
       </Grid>
 
       <Grid container mt={"40px"}>
         <Projects />
+      </Grid>
+      <Grid container mt={"40px"}>
+        <Footer />
       </Grid>
     </Grid>
   );
